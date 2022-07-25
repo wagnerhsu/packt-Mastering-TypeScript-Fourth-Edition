ea904c9da79ed0b5b6c52e314ce5e9d440281715
Wagner Xu <wagnerhsu@hotmail.com>
2022-07-25 11:02:20 +0800

    Test window.myVersion

--- MyCode/07-IntegrationWithJavaScript/Demo03/src/main.ts
+++ MyCode/07-IntegrationWithJavaScript/Demo03/src/main.ts
@@ -1,4 +1,8 @@
+/*eslint-disable @typescript-eslint/triple-slash-reference*/
+/// <reference path="../types/window.d.ts" />
+
 import { createApp } from "vue";
 import App from "./App.vue";
 
+console.log(window.myVersion);
 createApp(App).mount("#app");
